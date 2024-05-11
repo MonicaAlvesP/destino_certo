7adfefe9b8ea221aafee922f979ebaaee91bc757
Tulani <tulanidepaula@gmail.com>
2024-05-11 15:41:55 -0300

    merdaaaaaaaaaaaaaaaaaaaaaaaaa

--- src/Components/Ajuda/Ajuda.jsx
+++ src/Components/Ajuda/Ajuda.jsx
@@ -2,6 +2,7 @@ export default function Ajuda() {
   return (
     <div>
       <h1>Aqui ficará a página de Ajuda</h1>
+      <p> era uma sugestão de colocar em inglê<s>mas meu coração está com ódio!</s></p>
     </div>
   )
 }
